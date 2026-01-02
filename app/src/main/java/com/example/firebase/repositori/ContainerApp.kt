7a036a7fbda0e0dcb52b7fd7fc9b5d11d4d80ca3
aTaruna <aTaruna@users.noreply.github.com>
2026-01-02 19:18:31 +0700

    membuat class value

--- app/src/main/java/com/example/firebase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/firebase/repositori/ContainerApp.kt
@@ -5,3 +5,15 @@ import android.app.Application
 interface ContainerApp {
     val repositorySiswa: RepositorySiswa
 }
+class DefaultContainerApp : ContainerApp{
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
+class AplikasiDataSiswa : Application(){
+    lateinit var container: ContainerApp
+    override fun onCreate(){
+        super.onCreate()
+        this.container = DefaultContainerApp()
+    }
+}
