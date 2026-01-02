8b2288d579086cbd65463a8e4ced99acf948a3b7
aTaruna <aTaruna@users.noreply.github.com>
2026-01-02 19:18:05 +0700

    membuat interface value

--- app/src/main/java/com/example/firebase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/firebase/repositori/ContainerApp.kt
@@ -1,17 +1,7 @@
 package com.example.firebase.repositori
 
+import android.app.Application
+
 interface ContainerApp {
     val repositorySiswa: RepositorySiswa
-}
-class DefaultContainerApp : ContainerApp{
-    override val repositorySiswa: RepositorySiswa by lazy {
-        FirebaseRepositorySiswa()
-    }
-}
-class AplikasiDataSiswa : Application(){
-    lateinit var containerApp: ContainerApp
-    override fun onCreate(){
-        super.onCreate()
-        this.container = DefaultContainerApp()
-    }
 }
